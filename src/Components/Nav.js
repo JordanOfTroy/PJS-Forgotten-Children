e0ad0b069e88ce58bbd92758ae6b36c475c4aef2
Jordan Smithson <jordantroysmithson@gmail.com>
2019-02-11 14:28:43 -0700

    Nav links initially set. Will most likely change when final designs come through.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
+import {Link, withRouter} from 'react-router-dom'
 
 class Nav extends Component {
   render () {
     return(
       <div>
         <h1>Nav.js</h1>
+        <Link to = '/'>Home</Link>
+        <Link to = '/history'>Who We Are</Link>
+        <Link to = '/volunteer'>What We Do</Link>
       </div>
     )
   }
