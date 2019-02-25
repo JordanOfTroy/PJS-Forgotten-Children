761f8bb1c302ab72bfc0da3dbf05c3462efd3b38
Jordan Smithson <jordantroysmithson@gmail.com>
2019-02-25 00:55:56 -0700

    found two NPM packages for potential text editors. Not sure which to go with yet. Will need to look into both a bit more.

---
+++ Potential_Text_Editors.js
@@ -0,0 +1,24 @@
+// npm i react-draft-wysiwyg
+/**This one seeme pretty good, but it doesn't seem to have
+ * a built-in spell-checker.
+ */
+
+ // npm i react-quill
+ /** This one is very nice as well and come with a spell-checker.
+  * HOWEVER, I tried to upload a photo on the demo site twice, 
+  * and both times it bomb'd out. SO that may may be a problem.
+  * 
+  * I do like that on the demo site it shows you the HTML that is 
+  * being created as you type.
+  */
+
+
+  /** Going with either one of these should produce reliable results.
+   * They are both fairly new and look to be well used and supported.
+   * I am most likely going to need to set up an AWS account for PJs 
+   * to store the photos...... But maybe not. I will have to wait and \
+   * see how these two work. There may be a way to keep them in the server. 
+   * But it would be cool to set up and use and AWS too. 
+   * 
+   */
+
